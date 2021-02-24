3a13e6d0e65c697f4e881c475bc59556c8ca13b0
atusan <atunaseri@gmail.com>
2021-02-23 20:26:36 -0800

    javascript updated

--- js/main.js
+++ js/main.js
@@ -1,51 +1,70 @@
 //constant
-const imagesList = [
+const cardLists = [
     {
         src: 'imgs/one.svg',
-        name: 'one'
+        name: 'one',
+        flip: false,
+        facedown:'hidden'
     },
     {
         src: 'imgs/two.svg',
-        name: 'two'
+        name: 'two',
+        flip: false,
+        facedown:true
     },
     {
         src: 'imgs/three.svg',
-        name: 'three'
+        name: 'three',
+        flip: false,
+        facedown:true
     },
     {
         src: 'imgs/four.svg',
-        name: 'four'
+        name: 'four',
+        flip: false,
+        facedown:true
     }, 
     {
         src: 'imgs/one.svg',
-        name: 'one'
+        name: 'one',
+        flip: false,
+        facedown:true
     },
     {
         src: 'imgs/one.svg',
-        name: 'one'
+        name: 'one',
+        flip: false,
+        facedown:true
     },
    {
         src: 'imgs/three.svg',
-        name: 'three'
+        name: 'three',
+        flip: false,
+        facedown:true
     },
    {
         src: 'imgs/four.svg',
-        name: 'four'
+        name: 'four',
+        flip: false,
+        facedown:true
     }  
 ]
-const imgSrcs = ['imgs/one.svg','imgs/one.svg','imgs/two.svg','imgs/two.svg','imgs/three.svg','imgs/three.svg','imgs/four.svg','imgs/four.svg'];
+
+// const imgSrcs = ['imgs/one.svg','imgs/one.svg','imgs/two.svg','imgs/two.svg','imgs/three.svg','imgs/three.svg','imgs/four.svg','imgs/four.svg'];
 
 let numberOfHearts;
+let chosenCards;
 
 
 // cached elements
 const msg = document.getElementById('result');
 const heartsNumber = document.getElementById('heartsImg');
-const imgsContainers = document.querySelector('.container');
-// const cards = document.querySelectorAll('.cards');
-// console.log(cards)
+const cardContainers = document.querySelector('.container');
+
 const btnRestart = document.querySelector('button');
 
+document.getElementById('cards')
+  .addEventListener('click', handleCardClick);
 
 
 init();
@@ -53,29 +72,52 @@ init();
 function init(){
     console.log('init is working')
     numberOfHearts = 3
+    openedCards = []
+    chosenCards = []
     
     render();
+   
 }
 
 function render(){
     
-    const shuffleCards = shuffle(imgSrcs);
-    console.log(shuffleCards)
-    console.log(shuffleCards.length)
-    for (let i = 0; i < shuffleCards.length; i++){
-        const img = document.createElement('img');
-        img.setAttribute('src',shuffleCards[i]);
-        imgsContainers.appendChild('img');
-
+    const cards = shuffle(cardLists);
+    for (let i = 0; i < cards.length; i++){
+        // console.log(cards[i].)
+        const imgCard = document.createElement('img');
+        console.log(cards[i].flip)
+        if (cards[i].flip){
+            imgCard.setAttribute('src',cards[i].src);
+            imgCard.setAttribute('class', 'open')
+            imgCard.setAttribute('class', 'disabled')
+    
+        }
+        else{
+            imgCard.setAttribute('src','imgs/back.svg');
+        }
+        imgCard.setAttribute('id',i);
+        cardContainers.appendChild(imgCard);
     }
-    console.log('img') 
+    console.log(cardContainers,'imgcontainer')
+     
+}
+function handleCardClick(e){
+    console.log('handleCardClick its working ')
+    console.log(e.target.id)
+    let id = e.target.id;
 
-
-
+    // if(!cardLists[e.target.id].flip){
+    //     cardLists[e.target.id].flip=true;
+        
+    // }
+    
+    console.log(cardLists[e.target.id].flip)
+    
+    
 
 
 }
-
+//shuflle function to randomize the array
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
   
