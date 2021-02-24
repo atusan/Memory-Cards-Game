31479b3911a46824058a6a323ec2e8521597b158
atusan <atunaseri@gmail.com>
2021-02-24 14:45:04 -0800

    javascript updated

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const cardLists = [
         src: 'imgs/one.svg',
         name: 'one',
         flip: false,
-        facedown:'hidden'
+        facedown:'true'
     },
     {
         src: 'imgs/two.svg',
@@ -49,29 +49,34 @@ const cardLists = [
         facedown:true
     }  
 ]
+// let cards = shuffle(cardLists);
 
 // const imgSrcs = ['imgs/one.svg','imgs/one.svg','imgs/two.svg','imgs/two.svg','imgs/three.svg','imgs/three.svg','imgs/four.svg','imgs/four.svg'];
+let cards;
 
 let numberOfHearts;
 let chosenCards;
+let count;
 
 
 // cached elements
-const msg = document.getElementById('result');
-const heartsNumber = document.getElementById('heartsImg');
-const cardContainers = document.querySelector('.container');
 
-const btnRestart = document.querySelector('button');
+const deck = document.getElementById('deck')
 
-document.getElementById('cards')
+const container = document.querySelector('.container');
+
+document.getElementById('cardDivs')
   .addEventListener('click', handleCardClick);
 
 
 init();
 
 function init(){
+    
     console.log('init is working')
+    cards = shuffle(cardLists);
     numberOfHearts = 3
+    count = 0
     openedCards = []
     chosenCards = []
     
@@ -80,43 +85,64 @@ function init(){
 }
 
 function render(){
-    
-    const cards = shuffle(cardLists);
-    for (let i = 0; i < cards.length; i++){
-        // console.log(cards[i].)
-        const imgCard = document.createElement('img');
-        console.log(cards[i].flip)
-        if (cards[i].flip){
-            imgCard.setAttribute('src',cards[i].src);
-            imgCard.setAttribute('class', 'open')
-            imgCard.setAttribute('class', 'disabled')
-    
-        }
-        else{
-            imgCard.setAttribute('src','imgs/back.svg');
-        }
-        imgCard.setAttribute('id',i);
-        cardContainers.appendChild(imgCard);
-    }
-    console.log(cardContainers,'imgcontainer')
      
+//     for (let i = 0; i < cards.length; i++){
+//         // console.log(cards[i].)
+//         const imgCard = document.createElement('img');
+//         console.log(cards[i].flip)
+//         if (cards[i].flip){
+//             imgCard.setAttribute('src',cards[i].src);
+//             imgCard.setAttribute('class', 'open')
+//             imgCard.setAttribute('class', 'disabled')
+//         }
+//         else{
+//             imgCard.setAttribute('src','imgs/back.svg');
+//         }
+//         imgCard.setAttribute('id',i);
+//         cardContainers.appendChild(imgCard);
+//     }
+//     console.log(cardContainers,'imgcontainer')
+     
+// }
+for (let i = 0; i < cards.length; i++){
+    const card = document.createElement('div');
+    card.setAttribute('class','card');
+    card.dataset.name = cards[i].name;
+    card.style.backgroundImage = `url(${cards[i].src})`;
+    container.appendChild(card);
 }
+console.log(container)
+}
+
+
 function handleCardClick(e){
     console.log('handleCardClick its working ')
-    console.log(e.target.id)
-    let id = e.target.id;
+    console.log(e.target)
+    
+    let cardClicked = e.target;
+    chosenCards = [];
+    
+    if (count < 2) {
+        count++
 
-    // if(!cardLists[e.target.id].flip){
-    //     cardLists[e.target.id].flip=true;
+        cardClicked.classList.add('selected');
+        // cardClicked.classList.add('disable');
+        // console.log(cardClicked.dataset.name);
+        chosenCards.push(cardClicked);
+        console.log(chosenCards,'chosencards')
+
+        if (chosenCards[0].dataset.name === chosenCards[1].dataset.name){
+                    match();
+                }
+                else{
+                    unmatch();
+                }
         
-    // }
-    
-    console.log(cardLists[e.target.id].flip)
-    
-    
+      }
+    console.log(chosenCards,'chosencards')
+    console.log(chosenCards[0].dataset.name)
+    }
 
-
-}
 //shuflle function to randomize the array
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
