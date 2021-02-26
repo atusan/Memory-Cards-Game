9910870c4341ebbe819c89dfa78e42c7291429f1
atusan <atunaseri@gmail.com>
2021-02-25 16:49:21 -0800

    added flip to the cards when click happens

--- js/main.js
+++ js/main.js
@@ -1,49 +1,135 @@
 //constant
-const cardLists = [
-    {
-        src: 'imgs/one.svg',
-        name: 'one',
-        back:'imgs/back.svg'
-    },
-    {
-        src: 'imgs/two.svg',
-        name: 'two',
-        back:'imgs/back.svg'
-    },
-    {
-        src: 'imgs/three.svg',
-        name: 'three',
-        back:'imgs/back.svg'
-    },
-    {
-        src: 'imgs/four.svg',
-        name: 'four',
-        back:'imgs/back.svg'
-    }, 
-    {
-        src: 'imgs/one.svg',
-        name: 'one',
-        back:'imgs/back.svg'
-    },
-    {
-        src: 'imgs/two.svg',
-        name: 'two',
-        back:'imgs/back.svg'
-    },
-   {
-        src: 'imgs/three.svg',
-        name: 'three',
-        back:'imgs/back.svg'
-    },
-   {
-        src: 'imgs/four.svg',
-        name: 'four',
-        back:'imgs/back.svg'
-    }  
-]
-// let cards = shuffle(cardLists);
+// const cardLists = [
+//     {
+//         src: 'imgs/one.svg',
+//         name: 'one',
+//         back:'imgs/back.svg'
+//     },
+//     {
+//         src: 'imgs/two.svg',
+//         name: 'two',
+//         back:'imgs/back.svg'
+//     },
+//     {
+//         src: 'imgs/three.svg',
+//         name: 'three',
+//         back:'imgs/back.svg'
+//     },
+//     {
+//         src: 'imgs/four.svg',
+//         name: 'four',
+//         back:'imgs/back.svg'
+//     }, 
+//     {
+//         src: 'imgs/one.svg',
+//         name: 'one',
+//         back:'imgs/back.svg'
+//     },
+//     {
+//         src: 'imgs/two.svg',
+//         name: 'two',
+//         back:'imgs/back.svg'
+//     },
+//    {
+//         src: 'imgs/three.svg',
+//         name: 'three',
+//         back:'imgs/back.svg'
+//     },
+//    {
+//         src: 'imgs/four.svg',
+//         name: 'four',
+//         back:'imgs/back.svg'
+//     }  
+// ]
 
-// const imgSrcs = ['imgs/one.svg','imgs/one.svg','imgs/two.svg','imgs/two.svg','imgs/three.svg','imgs/three.svg','imgs/four.svg','imgs/four.svg'];
+const cardLists = [
+        {
+            src: 'imgs/cactus.png',
+            name: 'cactus',
+            back:'imgs/back.png',
+            faceDown: true,
+            cardId: 0
+        },
+        {
+            src: 'imgs/fish.png',
+            name: 'fish',
+            back:'imgs/back.png',
+            faceDown: true,
+            cardId: 1
+        },
+        {
+            src: 'imgs/ladybug.png',
+            name: 'ladybug',
+            back: 'imgs/back.png',
+            faceDown: true,
+            cardId: 2
+        },
+        {
+            src: 'imgs/rain.png',
+            name: 'rain',
+            back:'imgs/back.png',
+            faceDown: true,
+            cardId: 3
+        }, 
+        {
+            src: 'imgs/rainbow.png',
+            name: 'rainbow',
+            back:'imgs/back.png',
+            faceDown: true,
+            cardId: 4
+        },
+        {
+            src: 'imgs/sun.png',
+            name: 'sun',
+            back:'imgs/back.png',
+            faceDown: true,
+            cardId: 5
+        },
+        {
+            src: 'imgs/cactus.png',
+            name: 'cactus',
+            back:'imgs/back.png',
+            faceDown: true,
+            cardId: 6
+        },
+        {
+            src: 'imgs/fish.png',
+            name: 'fish',
+            back:'imgs/back.png',
+            faceDown: true,
+            cardId: 7
+        },
+        {
+            src: 'imgs/ladybug.png',
+            name: 'ladybug',
+            back: 'imgs/back.png',
+            faceDown: true,
+            cardId: 8
+        },
+        {
+            src: 'imgs/rain.png',
+            name: 'rain',
+            back:'imgs/back.png',
+            faceDown: true,
+            cardId: 9
+        }, 
+        {
+            src: 'imgs/rainbow.png',
+            name: 'rainbow',
+            back:'imgs/back.png',
+            faceDown: true,
+            cardId: 10
+        },
+        {
+            src: 'imgs/sun.png',
+            name: 'sun',
+            back:'imgs/back.png',
+            faceDown: true,
+            cardId: 11
+        }
+    ]
+
+
 let cards;
 
 let numberOfHearts;
@@ -80,39 +166,50 @@ function init(){
 }
 
 function render(){
-
-// for (let i = 0; i < cards.length; i++){
-//     const card = document.createElement('div');
-//     card.setAttribute('class','card');
-//     card.dataset.name = cards[i].name;
-//     card.style.backgroundImage = `url(${cards[i].src})`;
-//     container.appendChild(card);
-// }
-for (let i = 0; i < cards.length; i++){
+    container.innerHTML="";
+    for (let i = 0; i < cards.length; i++){
         const card = document.createElement('div');
         card.setAttribute('class','card');
         card.dataset.name = cards[i].name;
 
-        const front = document.createElement('div')
-        front.setAttribute('class' , 'front');
-        front.style.backgroundImage = `url(${cards[i].back})`;
+        console.log(cards[i].faceDown,'facedown status')
 
-        const back = document.createElement('div')
-        back.setAttribute('class' , 'back');
-        back.style.backgroundImage = `url(${cards[i].src})`;
+        if(cards[i].faceDown)
+         {
+            const front = document.createElement('div')
+            front.setAttribute('class' , 'front');
+            front.style.backgroundImage = `url(${cards[i].back})`;
+            front.dataset.cardId = cards[i].cardId;
+            front.dataset.name = cards[i].name;
+            card.appendChild(front);
+         }
+        else
+         {
+            const back = document.createElement('div')
+            back.setAttribute('class' , 'back');
+            back.dataset.cardId = cards[i].cardId;
+            back.dataset.name = cards[i].name;
+            back.style.backgroundImage = `url(${cards[i].src})`;
+            card.appendChild(back);
+         }
+    
+        container.appendChild(card);
+        
        
-        container.appendChild(card)
-        card.appendChild(front)
-        card.appendChild(back)
-  }
+    }
 }
 
 
 function handleCardClick(e){
-    // console.log('handleCardClick its working ')
-    // console.log(e.target)
+    console.log('handleCardClick its working ')
     
-    let cardClicked = e.target;
+    let cardClicked = cards.find(card => card.cardId === parseInt(e.target.dataset.cardId));
+    
+    
+    cardClicked.faceDown = false;
+    
+    console.log(cardClicked,'cardClicked')
+    
 
     if (cardClicked.nodeName === 'SECTION') {
         return}
@@ -121,25 +218,27 @@ function handleCardClick(e){
     if (count < 2) {
         count++
         
-        cardClicked.classList.add('selected');
+        // cardClicked.classList.add('selected');
+        // cardClicked.style.backgroundImage = e.target.id.src;
         
-        chosenCards.push(cardClicked);
+        // chosenCards.push(cardClicked);
     }
         // comparing two item 
-        console.log(chosenCards.length,'chosenCards length')
-        if (count === 2 ){
-            console.log(chosenCards[0].parentNode.dataset.name,'chosencards number 1')
-            console.log(chosenCards[1].parentNode.dataset.name,'chosencards number 2')
-            if (chosenCards[0].parentNode.dataset.name === chosenCards[1].parentNode.dataset.name){
-                    // match();
-                    setTimeout(match, delay)
-                }
-                else{
-                    // unmatch();
-                    setTimeout(unmatch, delay)
-                }
-        }
-        
+        // console.log(chosenCards.length,'chosenCards length')
+        // if (count === 2 ){
+        //     console.log(chosenCards[0].parentNode.dataset.name,'chosencards number 1')
+        //     console.log(chosenCards[1].parentNode.dataset.name,'chosencards number 2')
+        //     if (chosenCards[0].parentNode.dataset.name === chosenCards[1].parentNode.dataset.name){
+        //             // match();
+        //             setTimeout(match, delay)
+        //         }
+        //         else{
+        //             // unmatch();
+        //             setTimeout(unmatch, delay)
+        //         }
+        // }
+
+       render(); 
     
     }
 function match(){
